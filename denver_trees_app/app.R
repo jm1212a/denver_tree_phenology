c41057f47957ec3f19be5d2924395722acf806fb
jm1212a <jm1212a@american.edu>
2025-10-01 18:48:55 -0400

    diameter adjusted in Denver Trees

--- denver_trees_app/app.R
+++ denver_trees_app/app.R
@@ -1,33 +1,31 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
 
 library(shiny)
+library(tidyverse)
+
+denver_tree_sf <- read_rds("../data_raw/denver_tree/denver_tree_clean.rds")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Denver Trees Exploritory Analyis Application"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          dateRangeInput("data_range", "Select a Tree Inventrory Range?", 
+                         start = min(denver_tree_sf$inventory_date),
+                         end = max(denver_tree_sf$inventory_date),
+                         min = min(denver_tree_sf$inventory_date),
+                         max = max(denver_tree_sf$inventory_date)
+                         ),
+          varSelectInput("sci_species", )
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("map_denver")
         )
     )
 )
@@ -36,14 +34,7 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
     })
 }
 
